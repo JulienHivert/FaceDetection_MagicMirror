d98df49a940f868a85ea9e0655ab7b3e306eb7d4
JulienHivert <julienhivert.tt@gmail.com>
2018-10-12 15:31:51 +0200

    [FT] Save and move

--- face_detection.py
+++ face_detection.py
@@ -3,32 +3,42 @@
 import cv2, os, time, sys
 import numpy as np 
 import pickle
+import shutil 
 
 #cv2 pour l'utilisation de la webcam
 #os pour pouvoir save les images tests
 #numpy pour faire des calculs matriciels
 #time pour gerer le temps et l'ajouter dans le nom pour le trainning
+#shutil pour deplacer les photos dans un repertoire
 
 #Utilsation des patterns pour les haars
 face_cascade =  cv2.CascadeClassifier('haar/haarcascade_frontalface_default.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 #On recupere le temps courant 
 localtime = time.asctime((time.localtime(time.time())))
-print(localtime)
+#print(localtime)
+
+final_path ="/home/julien/python/OpenCV/Script/Mirroir_Connecté/images/"
+
+
 #On lit le modèle entrainé
 recognizer.read("trainner.yml")
 labels = {"person_name": 1}
+
 with open('label.pickle', "rb") as f:
     og_labels = pickle.load(f)
     labels = {v:k for k,v in og_labels.items()}
+
 #initialisation de la video
 cap = cv2.VideoCapture(0)
 #Ouverture de la vidéo 
+
 while (cap.isOpened()):
     #decoupage frame par frame
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+
     for (x,y,w,h) in faces:
         #Affiche les postion x,y,w,h du visage en fonction du frontalface pattern    
         #print(x,y,w,h)
@@ -59,6 +69,15 @@ while (cap.isOpened()):
                 img_item =  name+".png"
                 print(img_item)
                 cv2.imwrite(img_item, img)
+                origin_path ="/home/julien/python/OpenCV/Script/Mirroir_Connecté/"
+                folder_list = os.listdir(final_path)
+                for x in folder_list:
+                    print(x)
+                    if x == "Julien_hivert":
+                        print("true")
+                    else : 
+                        print("false")
+                #shutil.move(origin_path+img_item, final_path)
                 sys.exit()
             else :
                 print("salut")
@@ -66,7 +85,9 @@ while (cap.isOpened()):
             #Affichage du resultat
             #cv2.imwrite(img_item, roi_color)
     cv2.imshow("frame", img)
+    
     if cv2.waitKey(30) & 0xFF ==ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
